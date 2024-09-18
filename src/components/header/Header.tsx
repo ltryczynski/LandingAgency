4453db3fc39b50909d65374f375f2b93d2357268
krzemu <ltryczynski1@gmail.com>
2024-09-18 17:53:08 +0200

    Updated npm packages, fix layout error in header and contactSection

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -14,24 +14,6 @@ export default function Header() {
         <div className="text-purple text-2xl font-black">
           <Image src="/logo.svg" width={200} height={60} alt="Agency logo" />
         </div>
-        <nav>
-          <ul className="gap-12 hidden md:flex">
-            {navList.map((item) => (
-              <li key={item.name}>
-                <a href={item.link}>{item.name}</a>
-              </li>
-            ))}
-          </ul>
-          <Burger />
-
-          {/* <div className="mobileNav w-full bg-white h-full fixed inset-0 m-auto flex justify-end">
-            />
-            <div
-              className={`w-3/4 bg-purple h-full absolute left-auto inset-y-0 right-0 ${
-                openMobileNavState ? "block" : "hidden"
-              }`}></div>
-          </div> */}
-        </nav>
       </div>
     </header>
   );
--- src/components/sections/ContactSection.tsx
+++ src/components/sections/ContactSection.tsx
@@ -8,23 +8,20 @@ export default function ContentSection() {
   return (
     <section className="py-16">
       <SectionInner className="max-w-small text-center flex flex-col gap-16">
-        <H2 className="mask before:hidden text-center md:text-left">{`Let's Talk About the Next big thing`}</H2>
+        <H2 className="mask before:hidden text-center">{`Let's Talk About the Next big thing`}</H2>
         <div className="flex justify-center flex-wrap gap-8 border-b pb-16">
           <ButtonAlt>noreply@test.com</ButtonAlt>
           <ButtonAlt>+48 123 456 789</ButtonAlt>
         </div>
         <div className="flex justify-center flex-wrap gap-4 md:gap-8  pb-16 border-b">
           {socialTags.map((item) => (
-            <article
-              key={item.name}
-              className="bg-grayC px-4 py-2 rounded-full">
+            <article key={item.name} className="bg-grayC px-4 py-2 rounded-full">
               <a href={item.link}>{item.name}</a>
             </article>
           ))}
         </div>
         <div className="">
-          © 2024 Betheme template remake in NextJS | All Rights Reserved |
-          Created by{" "}
+          © 2024 Betheme template remake in NextJS | Created by{" "}
           <a href="https://lt-media.pl/" target="blank" className="text-purple">
             LTmedia
           </a>
